ff2efa999d2ce08d35d03e91a2f417b43307027a
muhammedzaigo <muhammed@zaigoinfotech.com>
2024-08-10 16:01:42 +0530

    removed app.filename in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
 
-from aws_connect import AWSConnection
-from s3_connect import S3Connection
-from lambda_connect import LambdaConnection
+from app.aws_connect import AWSConnection
+from app.s3_connect import S3Connection
+from app.lambda_connect import LambdaConnection
