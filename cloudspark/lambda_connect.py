134b5b6eb1c65aebe7dc5cf5a655006eb76ef442
muhammedzaigo <muhammed@zaigoinfotech.com>
2024-08-12 18:43:23 +0530

    verion change to 1.0.7 to 1.0.8

--- cloudspark/lambda_connect.py
+++ cloudspark/lambda_connect.py
@@ -1,5 +1,5 @@
 
-from aws_connect import AWSConnection
+from cloudspark.aws_connect import AWSConnection
 
 class LambdaConnection(AWSConnection):
     """A class to manage AWS S3 Lambda connection and operations."""
